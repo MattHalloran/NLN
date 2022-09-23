3b6ca301f7bc7aa27a415c13d65e2fd57f2b6b18
MattHalloran <matthalloran8@gmail.com>
2022-09-23 12:43:14 -0400

    Updated scripts and shared
    Shared package and scripts updated to latest

--- packages/shared/consts/src/api.ts
+++ packages/shared/consts/src/api.ts
@@ -2,6 +2,6 @@ import { ValueOf } from '.';
 
 // The length of a user session
 export const COOKIE = {
-    Session: "session-f234u7fdiafhdja2",
+    Jwt: "session-f234u7fdiafhdja2",
 } as const;
 export type COOKIE = ValueOf<typeof COOKIE>;
