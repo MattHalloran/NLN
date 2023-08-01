7116014eb9149b95e448f01e1edde56464f80d57
MattHalloran <matthalloran8@gmail.com>
2023-08-01 19:02:30 -0400

    Quality-of-life improvements
    - Package bumps
    - Set up server debugger
    - Added recommended VSCode extensions

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -4,7 +4,6 @@ import { Router } from '@shared/route';
 import { ApolloProvider } from '@apollo/client';
 import { initializeApollo } from 'graphql/utils/initialize';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 import { ErrorBoundary } from 'components';
 
 const client = initializeApollo();
@@ -36,4 +35,4 @@ serviceWorkerRegistration.register({
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
